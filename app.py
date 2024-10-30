443a2bf736985a510a2c258c7435d368a36fb85b
Kausar Ahmed <kausarahmed2nd@gmail.com>
2024-10-30 23:20:02 +0600

    added signup page

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("sign_up.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
