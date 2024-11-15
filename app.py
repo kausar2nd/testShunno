e05d9c7b9ed714fd72fa3e7abe98fc0a82d6b859
Kausar Ahmed <kausarahmed2nd@gmail.com>
2024-11-16 01:58:07 +0600

    added login and signup pages

--- app.py
+++ app.py
@@ -7,9 +7,31 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/signup")
+
+@app.route("/login")
 def signup():
-    return render_template("sign_up.html")
+    return render_template("all_login.html")
+
+
+@app.route("/user_login")
+def user_login():
+    return render_template("user_login.html")
+
+
+@app.route("/user_signup")
+def user_signup():
+    return render_template("user_signup.html")
+
+
+@app.route("/company_login")
+def company_login():
+    return render_template("company_login.html")
+
+
+@app.route("/company_signup")
+def company_signup():
+    return render_template("company_signup.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
