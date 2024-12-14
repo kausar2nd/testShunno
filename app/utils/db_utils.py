68b966a02c13c27344c43ce52ce27c7494629e42
Kausar Ahmed <kausarahmed2nd@gmail.com>
2024-12-14 20:58:01 +0600

    new UI

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -5,4 +5,6 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost", user="root", passwd="", database="alteruse_db"
     )
+    # host="localhost", user="jobairhossain_alteruse", passwd="alterUSE@123", database="jobairhossain_alteruse_db"
+
     return connection
