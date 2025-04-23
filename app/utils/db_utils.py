783111dc7047d46c2b6123d2942c563f3413e9ae
Kausar Ahmed <kausarahmed2nd@gmail.com>
2025-04-23 21:31:36 +0600

    added favicon and UI update

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -3,10 +3,7 @@ import mysql.connector
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="",
-        database="shunnowaste_db",
+        host="localhost", user="root", passwd="", database="shunnowaste_db",
     )
     # host="localhost", user="xrangqsu_shunnowaste", passwd="shunnowaste@", database="xrangqsu_shunnowaste_db"
     return connection
