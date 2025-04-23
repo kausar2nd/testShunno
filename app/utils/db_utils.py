fa0eebcac6e5814cfc62d563c38ab1ff535b85c0
Kausar Ahmed <kausarahmed2nd@gmail.com>
2025-04-23 21:34:18 +0600

    db fixed

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -3,7 +3,10 @@ import mysql.connector
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost", user="root", passwd="", database="shunnowaste_db",
+        host="localhost",
+        user="xrangqsu_shunnowaste",
+        passwd="shunnowaste@",
+        database="xrangqsu_shunnowaste_db",
     )
-    # host="localhost", user="xrangqsu_shunnowaste", passwd="shunnowaste@", database="xrangqsu_shunnowaste_db"
+    # host="localhost", user="root", passwd="", database="shunnowaste_db",
     return connection
